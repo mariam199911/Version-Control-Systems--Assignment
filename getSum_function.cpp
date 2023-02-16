e595b8f1945700177e74f51da5a722be189b653c
Mariam Reda <mmreda@ejada.com>
2023-02-16 11:32:55 +0200

    getAverage_function

--- getSum_function.cpp
+++ getSum_function.cpp
@@ -9,6 +9,12 @@ int getSum(const std::vector<int>& vec) {
     return sum;
 }
 
+double getAverage(const std::vector<int>& vec) {
+    int sum = getSum(vec);
+    double average = static_cast<double>(sum) / vec.size();
+    return average;
+}
+
 int main() {
     // create a vector of 10 integers
     std::vector<int> myVec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
@@ -18,6 +24,12 @@ int main() {
 
     // print the sum to the console
     std::cout << "The sum of the vector is: " << sum << std::endl;
+
+    // calculate the average of the vector
+    double average = getAverage(myVec);
+
+    // print the average to the console
+    std::cout << "The average of the vector is: " << average << std::endl;
 
     return 0;
 }
